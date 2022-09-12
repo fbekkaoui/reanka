fb6524ca4bd180b5ff5265eaf2af0fa53b4f04b8
start.vaadin.com <no-reply@vaadin.com>
2022-09-12 10:07:55 +0000

    Generated project

---
+++ src/main/java/com/fes/application/data/Role.java
@@ -0,0 +1,5 @@
+package com.fes.application.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/fes/application/data/service/SamplePersonRepository.java
@@ -0,0 +1,9 @@
+package com.fes.application.data.service;
+
+import com.fes.application.data.entity.SamplePerson;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SamplePersonRepository extends JpaRepository<SamplePerson, UUID> {
+
+}
